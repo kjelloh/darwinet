eb41f3483c27d2e25bf741a085c20dd3ecbb1f87
kjell-olov <kjell-olov@57849a7e-02c0-48c8-ab6e-32930a56f0c4>
2013-02-23 17:40:34 +0000

    Created Software/Common and Software/Projects folder structure. + Moved darwinet_bcb_1 to Software/Projects. + Removed DarwinetFramework (Now in Software/Common/cpp/DarwinetFramework + Updated darwinet_bcb_1 to use added DarwinetRADLib
    git-svn-id: https://svn.code.sf.net/p/darwinet/code/trunk@43 57849a7e-02c0-48c8-ab6e-32930a56f0c4

---
+++ Software/projects/darwinet_bcb_1/build/darwinet_bcb_1.cpp
@@ -0,0 +1,68 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file is part of and is developed by the Darwinet project at https://sourceforge.net/projects/darwinet/
+ */
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+#include <tchar.h>
+//---------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+USEFORM("Forms\MessagingFormUnit.cpp", MessagingForm);
+USEFORM("Forms\MIVFormUnit.cpp", MIVForm);
+USEFORM("Forms\MainFormUnit.cpp", MainForm);
+USEFORM("Forms\ApplicationFormUnit.cpp", ApplicationForm);
+USEFORM("..\..\..\common\cpp\DarwinetRADLib\ABOUT.cpp", AboutBox);
+USEFORM("..\..\..\common\cpp\DarwinetRADLib\BusinessLogFormUnit.cpp", BusinessLogForm);
+USEFORM("..\..\..\common\cpp\DarwinetRADLib\EditPropertiesFormUnit.cpp", EditPropertiesForm);
+USEFORM("..\..\..\common\cpp\DarwinetRADLib\EditPropertiesFrameUnit.cpp", EditApplicationPropertiesFrame); /* TFrame: File Type */
+//---------------------------------------------------------------------------
+WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
+{
+	try
+	{
+		Application->Initialize();
+		Application->MainFormOnTaskBar = true;
+		Application->CreateForm(__classid(TMainForm), &MainForm);
+		Application->CreateForm(__classid(TAboutBox), &AboutBox);
+		Application->CreateForm(__classid(TBusinessLogForm), &BusinessLogForm);
+		Application->CreateForm(__classid(TEditPropertiesForm), &EditPropertiesForm);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
+}
+//---------------------------------------------------------------------------
