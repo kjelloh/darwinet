13c711bdf3e61a395847a4ec5f31f09da3cec092
kjell-olov <kjell-olov@57849a7e-02c0-48c8-ab6e-32930a56f0c4>
2013-04-01 10:56:20 +0000

    Fixed misspelling of project MIVConsoleVS12. + renamed projects/MIVCosoleVS12 to projects/MIVConsoleVS12 - Still an empty Console.
    git-svn-id: https://svn.code.sf.net/p/darwinet/code/trunk@52 57849a7e-02c0-48c8-ab6e-32930a56f0c4

---
+++ Software/projects/MIVConsoleVS12/build/MIVConsole.cpp
@@ -0,0 +1,12 @@
+// MIVConsole.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "MIVConsoleCore.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
